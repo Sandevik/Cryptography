4ba84fce3775c3a7e997ae7a729bfd7777135bfe
Sandevik <62107622+Sandevik@users.noreply.github.com>
2023-02-26 18:51:14 +0100

    Logging

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace XOR
   {
     string word = "Encryption test";
     string key = "it works!"
-    Console.WriteLine( "\"" +word +"\"" +" encrypted: " + EncryptionDecryption.Encrypt(word, key) + "\n");
-    Console.WriteLine(EncryptionDecryption.Encrypt(word, key) +" decrypted: " + "\"" + XOR.Decrypt(XOR.Encrypt(word, key), key)+"\"");
+    Console.WriteLine( "\"" +word +"\"" +" encrypted: " + EncryptionDecryption.Encrypt(word, key) + "\n");   // Logs: "2c1a435|162|1f1a4e7|5454121c6|"
+    Console.WriteLine(EncryptionDecryption.Encrypt(word, key) +" decrypted: " + "\"" + XOR.Decrypt(XOR.Encrypt(word, key), key)+"\""); // Logs: "Encryption test"
   }
 }
