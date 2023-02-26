f3d77faecdf5a1ae73d7e8783867f6629240053e
Sandevik <62107622+Sandevik@users.noreply.github.com>
2023-02-26 19:17:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ namespace XOR
 {
   class Program
   {
-    static void Main(string args[]){
+    static void Main(string[] args){
       string word = "Encryption test";
       string key = "it works!"
       Console.WriteLine( "\"" +word +"\"" +" encrypted: " + EncryptionDecryption.Encrypt(word, key) + "\n");   // Logs: "2c1a435|162|1f1a4e7|5454121c6|"
