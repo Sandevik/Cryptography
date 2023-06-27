dc0e53215744e34e1393f0e81b778ff24c454b71
Sandevik <62107622+Sandevik@users.noreply.github.com>
2023-06-27 16:16:35 +0200

    Update Program.cs -> Rename EncryptionDecryption to Cryptography

--- Program.cs
+++ Program.cs
@@ -8,28 +8,28 @@ namespace XOR
         
       // Encrypt_1 & Decrypt_1 
         
-      Console.WriteLine( "\"" +word +"\"" +" encrypted: " + EncryptionDecryption.Encrypt_1(word, key) + "\n");
+      Console.WriteLine( "\"" +word +"\"" +" encrypted: " + Cryptography.Encrypt_1(word, key) + "\n");
       // Logs: "2c1a435|162|1f1a4e7|5454121c6|"
       
-      Console.WriteLine(EncryptionDecryption.Encrypt(word, key) +" decrypted: " + "\"" + EncryptionDecryption.Decrypt_1(EncryptionDecryption.Encrypt_1(word, key), key)+"\"");
+      Console.WriteLine(Cryptography.Encrypt(word, key) +" decrypted: " + "\"" + Cryptography.Decrypt_1(Cryptography.Encrypt_1(word, key), key)+"\"");
       // Logs: "Encryption test"
       
       /*--------------------------------------------------------------------------------------------------*/
       
       // Encrypt_2 & Decrypt_2
       
-      Console.WriteLine( "\"" +word +"\"" +" encrypted: " + EncryptionDecryption.Encrypt_2(word, key) + "\n");
+      Console.WriteLine( "\"" +word +"\"" +" encrypted: " + Cryptography.Encrypt_2(word, key) + "\n");
       // Logs: "e:GVQXTA;:TEWT"
       
-      Console.WriteLine(EncryptionDecryption.Encrypt(word, key) +" decrypted: " + "\"" + EncryptionDecryption.Decrypt_2(EncryptionDecryption.Encrypt_2(word, key), key)+"\"");
+      Console.WriteLine(Cryptography.Encrypt(word, key) +" decrypted: " + "\"" + Cryptography.Decrypt_2(Cryptography.Encrypt_2(word, key), key)+"\"");
       // Logs: "Encryption test"
       
       /*---------------------------------------------------------------------------------------------------*/
       
       // Encryption with both 
       
-       string encrypted = EncryptionDecryption.Encrypt_2(EncryptionDecryption.Encrypt_1(word, key), key);
-       string decrypted = EncryptionDecryption.Decrypt_1(EncryptionDecryption.Decrypt_2(encrypted, key), key);
+       string encrypted = Cryptography.Encrypt_2(Cryptography.Encrypt_1(word, key), key);
+       string decrypted = Cryptography.Decrypt_1(Cryptography.Decrypt_2(encrypted, key), key);
       
        Console.WriteLine( "\"" +word +"\"" +" encrypted: " + encrypted + "\n");
        // Logs: "GILLBIELGL"
